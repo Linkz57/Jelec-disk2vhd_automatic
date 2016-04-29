ba9df3b6892cc262334eb3c1dc1fe9f07f77ac68
Tyler Francis <Linkz57@gmail.com>
2016-04-29 15:01:35 -0500

    more efficient, and even starts with a #!

--- disk2vhd_cleanup.sh
+++ disk2vhd_cleanup.sh
@@ -1,17 +1,24 @@
+#!/bin/bash
+
 ## disk2vhd_cleanup.sh
-## vserion 0.1
+## version 0.5
 
-## 1
-ls -tp /mnt/disk2vhd/1 | grep -v '/$' | tail -n +6 | mail -s "I think we should delete these old VHD files for 1" tyler.francis@jelec.com
+\rm /mnt/disk2vhd/clean.log
 
-## 2
-ls -tp /mnt/disk2vhd/2 | grep -v '/$' | tail -n +6 | mail -s "I think we should delete these old VHD files for 2" tyler.francis@jelec.com
+function clean {
+	cd /mnt/disk2vhd/$1
+	echo $1 >> /mnt/disk2vhd/clean.log
+	echo "________" >> /mnt/disk2vhd/clean.log
+	ls -tp | grep -v '/$' | tail -n +6 >> /mnt/disk2vhd/clean.log
+	echo "" >> /mnt/disk2vhd/clean.log
+	echo "" >> /mnt/disk2vhd/clean.log
+	echo "" >> /mnt/disk2vhd/clean.log
+}
 
-## 3
-ls -tp /mnt/disk2vhd/3 | grep -v '/$' | tail -n +6 | mail -s "I think we should delete these old VHD files for 3" tyler.francis@jelec.com
+clean 1
+clean 2
+clean 3
+clean 4
+clean 5
 
-## 4
-ls -tp /mnt/disk2vhd/4 | grep -v '/$' | tail -n +6 | mail -s "I think we should delete these old VHD files for 4" tyler.francis@jelec.com
-
-## 5
-ls -tp /mnt/disk2vhd/5 | grep -v '/$' | tail -n +6 | mail -s "I think we should delete these old VHD files for 5" tyler.francis@jelec.com
+cat /mnt/disk2vhd/clean.log | mail -s "I want to delete these old VHD files" tyler.francis@jelec.com
