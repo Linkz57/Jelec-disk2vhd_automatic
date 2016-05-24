e3c21f7b0b844a4d4fbd2636bb2c09ddb110bdcf
Tyler Francis <Linkz57@gmail.com>
2016-05-24 13:26:54 -0500

    Many things! Fail if low space & email if too fast
    A reasonably big update, so I figured it deserved a major increment. The free space check is terrible, but probably functional. I haven't actually tested it. Recently a few disk2vhd attempts have silently failed due to low disk space, so I added the space check and the elapsed time check both fire off an email if they fail.
    
    Thankfully, the cleanup script triggers on the number of files in a directory, and not the amount of space available, so no harm came of missing a few disk2vhd triggers.

--- disk2vhd_automatic.bat
+++ disk2vhd_automatic.bat
@@ -1,6 +1,6 @@
 @echo off
 REM disk2vhd_automatic.bat
-REM version 2.9
+REM version 3.0
 REM written by Tyler Francis for JelecUSA on 2015-09-08
 REM this script is designed to aid a phase in automating a cheap backup system that will perform live, full-metal backups that are then able to be quickly virtualized at a moment's notice.
 echo.
@@ -51,12 +51,17 @@ mkdir "\\OMITTED\Shares\vhd\disk2vhd\%host%"
 
 REM log backup attempt and create something to test against before actual backup
 echo Backup's log >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 echo Stardate %date:~-4,4%/%date:~-10,2%/%date:~-7,2% >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 echo Startime %hr%:%time:~3,2%:%time:~6,2% >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 echo It begins >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 echo. >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 
-REM test backup destination for a successful write. If failed, email the authorities and make a local note of failure.
+REM test backup destination to make sure it exists. If failed, email the authorities and make a local note of failure.
 if exist "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log" (
 	set accessibility=true
 ) else (
@@ -64,6 +69,18 @@ if exist "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log" (
 	set actualerror="<p>The intended destination was \\OMITTED\Shares\vhd\disk2vhd\%host%<br />But I could not find it.</p> I spoke into the void and said Hello hello hello... Can anybody navigate there? Just reply if you can ACK me. Is there anynas home?<br />But there is no ping I am receiving. A distant share out on the network. It's only coming through in dropped packets. My SYNs send, but it can't ACK what I'm saying."
 	goto fail
 )
+
+REM test backup destination for available free space. Unfortunately, this only works if the destination is a Windows machine using a single volume for VHD storage.
+wmic /node:"OMITTED" LogicalDisk Where DeviceID="E:" Get FreeSpace | find /V "FreeSpace" > temp.txt
+set /P availableSpace=<temp.txt
+del temp.txt
+REM Since this language is stupid, I can't math large numbers. Instead I'm going to chop the last 6 digits off (plus the two line ending characters I think) and math this grossly rounded number instead.
+REM I want to make sure I have at least 55.6 GiB free, which should return true or fail from 32-bit integer stupidity If I have enough available.
+set chopSpace=%availableSpace:~0,-8%
+if $chopSpace% LSS 59701 (
+	set actualerror=There might not be enough space to store a new VHD, so I didn't try. You might be able to fit one more VHD in there, but come on. You have less than 56 GigaBytes of free space! You can't expect me to work in these conditions. You literatly have %availableSpace% Bytes available. Not a lot.
+	goto fail
+) 
 
 
 del dpart_start.txt
@@ -108,7 +125,7 @@ if %host%==OMITTED (
 	set mahpart=2
 	set provisioncheck=true
 )
-if %host%==tfrancis--e6530-dell-lt (
+if %host%==OMITTED (
 	set mahpart=1
 	set provisioncheck=true
 )
@@ -128,7 +145,7 @@ if %rundiskpart%==true (
 )
 
 REM thanks to Aacini for time calculation code: https://stackoverflow.com/questions/9922498/calculate-time-difference-in-windows-batch-file
-REM Get start time:
+REM Get start time, as measured in centiseconds:
 for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
    set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
 )
@@ -146,7 +163,7 @@ echo sel disk 0 > dpart_end.txt
 echo sel part %mahpart% >> dpart_end.txt
 echo remove letter=b noerr >> dpart_end.txt
 diskpart /s dpart_end.txt
-timeout /3
+timeout /t 3
 del dpart_start.txt
 del dpart_end.txt
 
@@ -162,12 +179,23 @@ echo %hh%:%mm%:%ss%,%cc%
 
 if %accessibility%==true (
 	echo It finished on %date:~-4,4%/%date:~-10,2%/%date:~-7,2% at %hr%:%time:~3,2%:%time:~6,2% >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 	echo The whole System Reserved and C:\ backup took %hh% hours, %mm% minutes, and %ss% seconds to complete >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 	echo. >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 	echo. >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 	echo. >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
 ) else (
-	echo apparently I can't write what you're now reading >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo apparently I can't write what you're now reading. This is an illogical scenario. >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+	echo "<br />" >> "\\OMITTED\Shares\vhd\disk2vhd\%host%\backup.log"
+)
+
+REM notify the authorities if the backup took less than 3 minutes.
+if %elapsed% LSS 18000 (
+	set actualerror=%host% " either has less than a gig of used space, super fast disks, or something has gone wrong. <br />Now I may be a simple script, designed to do exactly what I'm told without thinking too much about it, but this backup seemed WAY too fast. <br /> I would recommend you try to boot from the VHD I just tried to make, just to make sure it works the way you want it to. No one wants a backup that fails silently, so this is me trying to make some noise. <br />If I'm wrong, and you're imaging SSDs because you don't care about their lifespan, then go ahead and edit this error threshold so I only pipe up when you want me to. You should be able to find it on line 196 of c:\disk2vhd_automatic.bat last time I checked. Things change, so ask around if you don't see it. Also, by C:\ I mean the local disk of " %host%
+	goto fail
 )
 
 @echo off
@@ -201,20 +229,29 @@ exit
 
 
 :fail
-blat OMITTED -subject "Lo! %host% has become error, destroyer of backup schedules." -html -body "%actualerror% <br /><br />When I was a subroutuine, I had a error; my STDOUT looked like two baloons. Now I've got that feeling once again. I can explain, you'd probably understand. This is now how I am: <br /> IIIIIIiiiiii... have become, uncomfortably erroneous.</p>"
 echo "Backup's log >> backup.log
+echo "<br />" >> backup.log
 echo Stardate %date:~-4,4%/%date:~-10,2%/%date:~-7,2% >> backup.log
+echo "<br />" >> backup.log
 echo Startime %hr%:%time:~3,2%:%time:~6,2% >> backup.log
+echo "<br />" >> backup.log
 echo I have failed. Woe is me, a wretch of a script! %actualerror% >> backup.log
+echo "<br />" >> backup.log
 echo When I was a subroutuine, I had a error. my STDOUT looked like two baloons. Now I've got that feeling once again. I can explain, you'd probably understand. This is now how I am >> backup.log
+echo "<br />" >> backup.log
 echo IIIIIIiiiiii... have become, uncomfortably erroneous. >> backup.log
 echo. >> backup.log
+echo "<br />" >> backup.log
+echo "<br />" >> backup.log
+echo "<br />" >> backup.log
 echo. >> backup.log
 echo. >> backup.log
+echo "<br />" >> backup.log
+type backup.log | blat -to tyler.francis@jelec.com -server 192.168.11.10 -f disk2vhd_automatic@jelec.com -subject "Lo! %host% has become error, destroyer of backup schedules. Also this email is backwards, the newest info is at the end" -html 
 echo.
 echo.
 echo.
 echo.
 echo whelp, this failed.
 timeout /t 120
-exit
+REM exit
