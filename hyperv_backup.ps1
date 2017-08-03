d965c31a837477ebcf0fe09291aceeb345d9d3e4
Tyler Francis <Linkz57@gmail.com>
2017-08-03 14:33:19 -0500

    Mediocre Hyper-V backup script
    Line 14 really leverages the benefits of both PowerShell and Hyper-V. Line 12 is on-par with Bash and GNU coreutils. Every other line of code has been included to work around the many shortcomings of PowerShell, Hyper-V, or Windows. It took me 3 days to write 8 lines, and a 4th day to Windows Task Scheduler working. PowerShell is great, and just needs a few more years of maturation. Windows will always be a stick in the mud. I hope Windows Server keeps losing market share so Microsoft will continue to improve PowerShell on Linux.

---
+++ hyperv_backup.ps1
@@ -0,0 +1,17 @@
+## hyperv_backup.ps1
+## Version 1.0
+## Basically, it exports all VMs that have notes reading Auto-Backuped-Up then it compresses the exports, then moves the archive to a Samba share and cleans up the local backups.
+
+
+## Thanks to Karl Glennon for the following 7-Zip function
+## https://stackoverflow.com/questions/1153126/how-to-create-a-zip-archive-with-powershell
+function create-7zip([String] $aDirectory, [String] $aZipfile){    
+    [string]$pathToZipExe = "$($Env:ProgramFiles)\7-Zip\7z.exe";    
+    [Array]$arguments = "a", "-tzip", "$aZipfile", "$aDirectory";    & $pathToZipExe $arguments;
+}
+$todayDate = $((get-date).tostring('yyy-MM-dd_HH-mm'))
+
+get-vm | where-object { $_.Notes -like "*Auto-Backuped-Up*" } | Export-VM -Path F:\auto_hyperv_backups\$todayDate\
+create-7zip "F:\auto_hyperv_backups\$todayDate\" "F:\auto_hyperv_backups\$todayDate.zip"
+move-item "F:\auto_hyperv_backups\$todayDate.zip" -destination "\\NAS\auto_hyperv_backups\" -Force
+remove-item -recurse "F:\auto_hyperv_backups\$todayDate"
